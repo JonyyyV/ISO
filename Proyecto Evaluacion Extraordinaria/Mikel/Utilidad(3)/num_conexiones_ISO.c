592e939a1e55ec3f70a73adcf87135ec24c3da09
Jon Ortiz <jontxuuv@gmail.com>
2021-06-18 10:40:37 +0200

    Utilidad (3) finalizada, falta comprobarla

--- Proyecto Evaluacion Extraordinaria/Mikel/Utilidad(3)/num_conexiones_ISO.c
+++ Proyecto Evaluacion Extraordinaria/Mikel/Utilidad(3)/num_conexiones_ISO.c
@@ -5,18 +5,21 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+// Declaramos antes las funciones para que no aparezca ningun tipo de warning
+int wait();
+int error();
+int mkfifo();
+
 void main(int argc, char *argv[])
 {
     int pid;
     if (mkfifo("my_fifo", 0644) == -1)
         error("mkfifo");
-    printf("parent: fifo created\n");
     switch (pid = fork())
     {
     case -1:
         error("fork");
-    case 0: /* 1st child process: who */
-        printf("1st child process created\n");
+    case 0:
         if (close(1) == -1)
             error("close");
         if (open("my_fifo", O_WRONLY) != 1)
@@ -24,25 +27,21 @@ void main(int argc, char *argv[])
         execlp("last", "last", NULL);
         error("execlp");
     }
-    printf("parent: who(%d) process launched\n", pid);
 
     switch (pid = fork())
     {
     case -1:
         error("fork");
-    case 0: /* 2nd child process: wc -l */
-        printf("2nd child process created\n");
+    case 0:
         if (close(0) == -1)
             error("close");
         if (open("my_fifo", O_RDONLY) != 0)
             error("open");
-        execlp("grep", "grep", "jony","-c", NULL);
+        execlp("grep", "grep", "acaf00", "-c", NULL);
         error("execlp");
     }
-    printf("parent: wc(%d) process launched\n", pid);
+
     while ((pid = wait(NULL)) != -1)
-        printf("parent: %d process finished\n", pid);
-    unlink("my_fifo");
-    printf("parent: fifo removed\n");
+        unlink("my_fifo");
     exit(0);
 }
